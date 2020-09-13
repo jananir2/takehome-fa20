4b1bf0a2e7faa0ff377292f44beee6710bdfc7c9
repl.it user <replituser@example.com>
2020-09-13 01:02:44 +0000

    Finished frontend part 1 and 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Instructions from './Instructions'
 import Restaurant from './Restaurant'
+import Counter from './Counter'
 
 class App extends Component {
   constructor(props) {
@@ -17,9 +18,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Instructions />
+        <Counter/>
+        <Instructions complete={true} />
         {this.state.restaurants.map(x => (
-          <Restaurant id={x.id} name={x.name} rating={x.rating} />
+          <Restaurant id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
       </div>
     )
