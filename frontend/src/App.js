104a35480cf3fa0d5bc7c16c13684f56cbb9556c
repl.it user <replituser@example.com>
2020-09-13 18:15:24 +0000

    Finish challenge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
         {id: 1, name: "Golden Harbor", rating: 10},
         {id: 2, name: "Potbelly", rating: 6},
         {id: 3, name: "Noodles and Company", rating: 8},
-      ]
+      ],
+      inputValue: ''
     }
   }
 
@@ -21,8 +22,37 @@ class App extends Component {
         {this.state.restaurants.map(x => (
           <Restaurant id={x.id} name={x.name} rating={x.rating} />
         ))}
+        <React.Fragment>
+          <form>
+            <label htmlFor="username">Add Restaurant</label>
+            <input
+              type="text"
+              name="restaurant"
+              value={this.state.inputValue}
+              onChange={this.updateInputValue}/>
+          </form>
+          <button onClick={this.handleChange}>Submit</button>
+        </React.Fragment>
       </div>
     )
+  }
+
+  updateInputValue = event => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
+  handleChange = () => {
+    const name = this.state.inputValue;
+    const id = this.state.restaurants.id + 1;
+    const rating = 0;
+    const info = {id: id, name: name, rating: rating};
+    const data = this.state.restaurants;
+    data.push(info);
+    this.setState({
+      data: data
+    });
   }
 }
 
